88a7395fc419e137ba38ca573921a70f2fa1b745
Boris <boris.vega.ieei@gmail.com>
2021-11-22 15:47:24 +0000

    first commit

---
+++ ARRAYinsideArray.js
@@ -0,0 +1,19 @@
+//ARRAY DECLARATION: 3 arrays within 1 
+let shoppingList =
+[
+
+    day1= ["milk" , "eggs"],
+    day2= ["sugar" , "salt"],
+    day3= ["bread" , "bananas"]
+
+];
+console.log(shoppingList);
+console.log(shoppingList[1][2]);
+
+let shoppingList1 = [
+    beauty = ["soap","clenser","face mask"],
+    grocery = ["lettuce","milk","bread"],
+    petCare = ["catfood","litter","toy"]
+    ]
+    console.log(shoppingList1);
+    console.log(shoppingList1[1][2])
